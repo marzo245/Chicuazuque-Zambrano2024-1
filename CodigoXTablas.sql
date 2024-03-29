5344b3af384dd2034f1ee797971748e9114c2c34
Diego <58009401+marzo245@users.noreply.github.com>
2024-03-28 20:30:00 -0500

    Create CodigoXTablas.sql

---
+++ CodigoXTablas.sql
@@ -0,0 +1,17 @@
+----Estructura y Restricciones Declarativas
+--XTablas. Eliminación de tablas
+DROP TABLE IF EXISTS Sesion;
+DROP TABLE IF EXISTS Gratis;
+DROP TABLE IF EXISTS Publicidad;
+DROP TABLE IF EXISTS Ve;
+DROP TABLE IF EXISTS Pago;
+DROP TABLE IF EXISTS Essujugadorfavorito;
+DROP TABLE IF EXISTS Essuequipofavorito;
+DROP TABLE IF EXISTS Jugador;
+DROP TABLE IF EXISTS Pertenece;
+DROP TABLE IF EXISTS Equipo;
+DROP TABLE IF EXISTS CuerpoTecnico;
+DROP TABLE IF EXISTS Partido;
+DROP TABLE IF EXISTS Liga;
+DROP TABLE IF EXISTS Clasificacion;
+DROP TABLE IF EXISTS Estadisticas;
