0c52d3d8d5185d2009aac77bc4d04f4b7e1fa7dd
Diego <58009401+marzo245@users.noreply.github.com>
2024-04-13 19:07:29 -0500

    Update AzulConsultas.sql

--- AzulConsultas.sql
+++ AzulConsultas.sql
@@ -27,3 +27,38 @@ INNER JOIN Partido P ON E.partidoCodigo = P.codigo
 WHERE E.equipo = 'NombreDelEquipo'
    AND (P.equipoLocal = 'NombreDelEquipo' OR P.equipoVisitante = 'NombreDelEquipo')
 GROUP BY E.equipo;
+
+--Consultas Operativas
+--Estado actual de la suscripción y tiempo de suscripción de un usuario específico:
+SELECT 
+    P.sesion,
+    CASE
+        WHEN P.fechaFin >= CURRENT_DATE THEN 'Activa'
+        ELSE 'Expirada'
+    END AS estado_suscripcion,
+    DATE_DIFF(CURRENT_DATE, P.fechaIncio, DAY) AS dias_suscrito
+FROM Pago P
+WHERE P.sesion = 'correo_del_usuario@example.com';
+
+--Rendimiento de todos los equipos en una temporada:
+SELECT 
+    C.equipo,
+    SUM(C.puntos) AS puntos_totales,
+    SUM(C.partidosGanados) AS partidos_ganados,
+    SUM(C.partidosEmpatados) AS partidos_empatados,
+    SUM(C.partidosPerdidos) AS partidos_perdidos
+FROM Clasificacion C
+GROUP BY C.equipo;
+
+Estadísticas de jugadores:
+SELECT 
+    J.nombre AS nombre_jugador,
+    E.equipo,
+    SUM(E.asistencias) AS total_asistencias,
+    SUM(E.tarjetasAmarillas) AS total_tarjetas_amarillas,
+    SUM(E.tarjetasRojas) AS total_tarjetas_rojas,
+    AVG(E.posesionLocal) AS promedio_posesion_local,
+    AVG(E.posesionVisitante) AS promedio_posesion_visitante
+FROM Estadisticas E
+JOIN Jugador J ON E.jugadorNit = J.nit AND E.jugadorTid = J.tid
+GROUP BY J.nombre, E.equipo;
