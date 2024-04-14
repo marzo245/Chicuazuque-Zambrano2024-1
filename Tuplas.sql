9db2c3d86ab4d8ce22894d87e828c484f3ac6c8d
juanzambranog <142639568+juanzambranog@users.noreply.github.com>
2024-04-13 22:09:30 -0500

    Restricciones Declarativas, Procedimentales y Automatización

---
+++ Tuplas.sql
@@ -0,0 +1,17 @@
+ALTER TABLE Pago
+ADD CONSTRAINT CK_Pago_FechaInicio_FechaFin
+CHECK(fechaFin>fechaInicio)
+
+ALTER TABLE Liga
+ADD CONSTRAINT CK_Liga_FechaInicio_FechaFin
+CHECK(fechaFin>fechaInicio)
+
+ALTER TABLE Partido
+ADD CONSTRAINT CK_Partido_GolLocal_GolVisitante
+CHECK(golesLocal>=0 AND golesVisitante >=0)
+
+ALTER TABLE Estadisticas
+ADD CONSTRAINT CK_Estadisticas_posesionLocal_posesionVisitante
+CHECK(posesionLocal>=0 AND posesionVisitante >=0)
+
+
