4bcc8f3e62858f03f7d3035da99650de4c497aff
juanzambranog <142639568+juanzambranog@users.noreply.github.com>
2024-05-25 23:48:09 -0500

    Update VerdeTuplas.sql

--- VerdeTuplas.sql
+++ VerdeTuplas.sql
@@ -1,17 +1,15 @@
 ALTER TABLE Pago
 ADD CONSTRAINT CK_Pago_FechaInicio_FechaFin
-CHECK(fechaFin>fechaInicio)
+CHECK(fechaFin>fechaIncio);
 
 ALTER TABLE Liga
 ADD CONSTRAINT CK_Liga_FechaInicio_FechaFin
-CHECK(fechaFin>fechaInicio)
+CHECK(fechaFin>fechaInicio);
 
 ALTER TABLE Partido
 ADD CONSTRAINT CK_Partido_GolLocal_GolVisitante
-CHECK(golesLocal>=0 AND golesVisitante >=0)
+CHECK(golesLocal>=0 AND golesVisitante >=0);
 
 ALTER TABLE Estadisticas
 ADD CONSTRAINT CK_Estadisticas_posesionLocal_posesionVisitante
-CHECK(posesionLocal>=0 AND posesionVisitante >=0)
-
-
+CHECK(posesionLocal>=0 AND posesionVisitante >=0);
