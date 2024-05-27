f9a533791b8536a6f66949f6b444a11962033a1d
Diego <58009401+marzo245@users.noreply.github.com>
2024-05-27 17:14:39 -0500

    Create AzulXPoblar.sql

---
+++ AzulXPoblar.sql
@@ -0,0 +1,50 @@
+-- Vaciar la tabla Aviso
+DELETE FROM Aviso;
+
+-- Vaciar la tabla Sesion
+DELETE FROM Sesion;
+
+-- Vaciar la tabla Gratis
+DELETE FROM Gratis;
+
+-- Vaciar la tabla Publicidad
+DELETE FROM Publicidad;
+
+-- Vaciar la tabla Ve
+DELETE FROM Ve;
+
+-- Vaciar la tabla Pago
+DELETE FROM Pago;
+
+-- Vaciar la tabla Tarjeta
+DELETE FROM Tarjeta;
+
+-- Vaciar la tabla Essujugadorfavorito
+DELETE FROM Essujugadorfavorito;
+
+-- Vaciar la tabla Essuequipofavorito
+DELETE FROM Essuequipofavorito;
+
+-- Vaciar la tabla Jugador
+DELETE FROM Jugador;
+
+-- Vaciar la tabla Pertenece
+DELETE FROM Pertenece;
+
+-- Vaciar la tabla Equipo
+DELETE FROM Equipo;
+
+-- Vaciar la tabla CuerpoTecnico
+DELETE FROM CuerpoTecnico;
+
+-- Vaciar la tabla Partido
+DELETE FROM Partido;
+
+-- Vaciar la tabla Liga
+DELETE FROM Liga;
+
+-- Vaciar la tabla Clasificacion
+DELETE FROM Clasificacion;
+
+-- Vaciar la tabla Estadisticas
+DELETE FROM Estadisticas;
